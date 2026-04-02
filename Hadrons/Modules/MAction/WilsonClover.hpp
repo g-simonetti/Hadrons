60df4139bf2e5c5a120c9135babae8aa53156d4b
g-simonetti <gianmarco.simonetti6@gmail.com>
2026-04-02 15:54:55 +0100

    Removing instantation for Sp4 Antisym. rep.

--- Hadrons/Modules/MAction/WilsonClover.hpp
+++ Hadrons/Modules/MAction/WilsonClover.hpp
@@ -77,10 +77,8 @@ public:
 };
 
 MODULE_REGISTER_TMP(WilsonClover, TWilsonClover<FIMPL>, MAction);
-MODULE_REGISTER_TMP(WilsonCloverAsym, TWilsonClover<SpWilsonTwoIndexAntiSymmetricImplR>, MAction);
 #ifdef GRID_DEFAULT_PRECISION_DOUBLE
 MODULE_REGISTER_TMP(WilsonCloverF, TWilsonClover<FIMPLF>, MAction);
-MODULE_REGISTER_TMP(WilsonCloverAsymF, TWilsonClover<SpWilsonTwoIndexAntiSymmetricImplF>, MAction);
 #endif
 
 /******************************************************************************
